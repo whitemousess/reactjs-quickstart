76809931112a8ba15bec18f7039eead13bc7663e
whitemousess <thangcoiofficial@gmail.com>
2024-01-16 15:12:45 +0700

    vite project

--- src/components/GlobalStyles/GlobalStyles.jsx
+++ src/components/GlobalStyles/GlobalStyles.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import './GlobalStyles.scss';
+import './GlobalStyles.css';
 
 function GlobalStyles({ children }) {
     return children;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
